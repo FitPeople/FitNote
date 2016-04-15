80402093fc922574eb60186e40987e27525ebee1
ghayrulla <ghayrullaghayrat@hotmail.com>
2016-04-14 20:08:12 -0400

    updated

---
+++ FitController.swift
@@ -0,0 +1,26 @@
+
+//
+//  FitController.swift
+//  FitNote
+//
+//  Created by Gary Ghayrat on 4/14/16.
+//  Copyright © 2016 FitPeople. All rights reserved.
+//
+
+import UIKit
+
+class FitController: UIViewController {
+
+    @IBOutlet weak var dailyAveg: UIImageView!
+    @IBOutlet weak var daysCount: UILabel!
+    @IBOutlet weak var daysLabel: UILabel!
+    @IBOutlet weak var mainTableView: UITableView!
+    @IBOutlet weak var activitiesPicker: UIPickerView!
+    
+    @IBOutlet weak var setsPicker: UIPickerView!
+    @IBOutlet weak var colonPicker: UIPickerView!
+    @IBOutlet weak var subsetPicker: UIPickerView!
+    @IBOutlet weak var submitButton: UIButton!
+    
+    
+}
